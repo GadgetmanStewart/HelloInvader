690e64cfd49fff857251774af5ecb21ffda76dd5
Stewart Clark <stewart.clark@prpsolutions.com>
2019-05-19 21:30:58 +0200

    Upd2

--- HelloInvader/HelloInvader/Program.cs
+++ HelloInvader/HelloInvader/Program.cs
@@ -6,7 +6,7 @@ namespace HelloInvader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Invader Branch1");
+            Console.WriteLine("Hello Invader Master");
             Console.WriteLine("Branch1");
         }
     }
